52bc4225d10218fc1ae1a413ad66d9706217539e
leobraboo <leonardo.l.souza@ufv.br>
2023-03-02 22:30:31 -0300

    Create estructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import { AppRoutes } from "./routes"
 
 export function App() {
 
   return (
-    <div className="App">
-      home page
-    </div>
+    <>
+      <AppRoutes />
+    </>
   )
 }
 
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import {Routes, Route } from "react-router-dom";
+import { Home } from "../page";
+
+
+export function AppRoutes() {
+  return(
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+    </Routes>
+  )
+}
